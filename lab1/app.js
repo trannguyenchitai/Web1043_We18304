1df6facf3a870c9891732e10951608816bd98c87
Tai Tran <taitncpd08025.edu.vn>
2023-03-09 21:09:43 +0700

    7-Add We18304

---
+++ lab1/app.js
@@ -0,0 +1,7 @@
+let a = prompt("Nhap so nguyen a: ");
+let a1 = parseInt(a);
+let b = prompt("Nhap so nguyen b:");
+let b1 = parseInt(b);
+let c = parseInt(a) + parseInt(b);
+document.write(c);
+console.log(c);
