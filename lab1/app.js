158e4773392ace91a907fc336419ae0deaa05342
Tai Tran <taitncpd08025.edu.vn>
2023-03-09 21:26:46 +0700

    lab1

--- lab1/app.js
+++ lab1/app.js
@@ -1,17 +1,10 @@
 let y = 10;
 //set y value of 10
 console.log();
-alert();
-
-var x = prompt("Input n: ");
-var x1 = parseInt(n);
-document.write(n);
-console.log(n);
+alert(y);
 
 let a = prompt("Nhap so nguyen a: ");
 let a1 = parseInt(a);
-let b = prompt("Nhap so nguyen b:");
-let b1 = parseInt(b);
-let c = parseInt(a) + parseInt(b);
+let c = parseInt(a);
 document.write(c);
 console.log(c);
