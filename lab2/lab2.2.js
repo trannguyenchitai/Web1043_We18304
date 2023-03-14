106e675921adac7926cfd0b5bcadaebb55900783
Tai Tran <taitncpd08025.edu.vn>
2023-03-14 15:56:34 +0700

    noplab

--- lab2/lab2.2.js
+++ lab2/lab2.2.js
@@ -1,19 +1,19 @@
 //nhap a,b,c
-let a = parseInt(prompt("moi nhap a"));
- let b = parseInt(prompt("moi nhap b"));
- let c = parseInt(prompt("moi nhap c"));
+let a = parseInt(prompt("Nhap a"));
+ let b = parseInt(prompt("Nhap b"));
+ let c = parseInt(prompt("Nhap c"));
  //kiemtra
  if(a+b>c && b+c>a && a+c>b){
     if(a*a==b*b+c*c || b*b==a*a+c*c || c*c== a*a+b*b){
-        document.write('Đay la tam giac vuong');
+        document.write('Day la tam giac vuong');
 
     }else if(a==b && b==c){
-        document.write('Đay la tam giac can');
+        document.write('Day la tam giac can');
     }else{
-        document.write('Đay la tam giac thưong');
+        document.write('Day la tam giac thuong');
     }
 
  }else{
-    document.write('ba cạnh a,b,c không phai la ba canh cua mot tam giac!');
+    document.write('Ba cạnh a,b,c không phai la ba canh cua mot tam giac!');
 
  }
