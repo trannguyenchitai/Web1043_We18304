9d9580ad3c62e5ef81447413690852c4d6197a05
Tai Tran <taitncpd08025.edu.vn>
2023-03-22 18:16:11 +0700

    noplab2

--- lab2/lab2.1.js
+++ lab2/lab2.1.js
@@ -7,12 +7,12 @@ if(a == 0){
 }else{
     let delta = Math.pow(b, 2) - 4 * a * c;
 
-    if (delta == 0){
+    if (delta < 0){
+        document.writeln("Phuong trinh vo nghiem");
+    }else if (delta == 0){
         const x = -b / (2 * a);
 
         document.writeln("Phuong trinh co nghiem kep: x = "+x);
-    }else if (delta < 0){
-        document.writeln("Phuong trinh vo nghiem");
     }else{
         document.writeln("Phuong trinh co 2 nghiem phan biet");
         const x1 = (-b + Math.sqrt(delta)) / (2 * a);
