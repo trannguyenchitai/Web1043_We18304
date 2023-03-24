cd97693e7a04857ff1ff951ce3d65290cb497738
Tai Tran <taitncpd08025.edu.vn>
2023-03-24 17:28:01 +0700

    noplab2

--- lab2/lab2.2.js
+++ lab2/lab2.2.js
@@ -1,8 +1,6 @@
-//nhap a,b,c
 let a = parseInt(prompt("Nhap a"));
  let b = parseInt(prompt("Nhap b"));
  let c = parseInt(prompt("Nhap c"));
- //kiemtra
  if(a+b>c && b+c>a && a+c>b){
     if(a*a==b*b+c*c || b*b==a*a+c*c || c*c== a*a+b*b){
         document.write('Đây là tam giác vuông');
